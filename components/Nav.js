6b7675a51499e2733f794c95010524905bdcd11d
ferdous85 <ferdous85@gmail.com>
2021-06-01 19:34:53 +0600

    cng

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,14 @@
+import request from "../utils/request"
+
 function Nav() {
     return (
         <nav>
-           <div></div>
+           <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-10 overflow-x-scroll scroollbar-hidden">
+               {Object.entries(request).map(([key, {title, url}]) =>(
+                    <h2 key={key} className="last:pr-4 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500" > {title} </h2>
+               ))}
+           </div>
+           <div />
         </nav>
     )
 }
--- utils/request.js
+++ utils/request.js
@@ -4,5 +4,45 @@ export default{
     fetchTrending : {
         title: 'Trending',
         url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTopRated : {
+        title: 'Top Rated',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchActionMovies : {
+        title: 'Action',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchComedyMovies : {
+        title: 'Comedy',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchHorrorMovies : {
+        title: 'Horror',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchRomanceMovies : {
+        title: 'Trending',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchMystery : {
+        title: 'Romance',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchSciFi : {
+        title: 'Sci Fi',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchWestern : {
+        title: 'Western',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchAnimation : {
+        title: 'Animation',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTV : {
+        title: 'TV',
+        url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
     }
 }
